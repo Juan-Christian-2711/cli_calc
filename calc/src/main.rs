4c1c398afcc62d39f7a633c12ac2dd7993b4776c
Juan Christian <Juanchristian2002@outlook.com>
2024-10-24 14:24:02 +0100

    Add in regex
    Adds in code which uses regex to parse user input

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -1,5 +1,5 @@
 use std::io;
-
+use regex::Regex;
 
 fn main() {
     let mut quit = false;
@@ -14,14 +14,22 @@ fn calc() -> bool {
     io::stdin()
         .read_line(&mut equation)
         .expect("Failed to read line");
+
     if equation == String::from("q\n"){
         println!("exiting program");
         return true;
     }
     
-    match operator {
-                
+    //split the string into Coefficients, and operators
+
+    let re = Regex::new(r"(\d+|[+\-*/()])").unwrap();
+    let mut equation_vec = Vec::new();
+
+    for mat in re.find_iter(equation.as_str()) {
+        equation_vec.push(&equation[mat.start()..mat.end()]);
     }
+
+    println!("{:?}", equation_vec);
 
     false
 }
